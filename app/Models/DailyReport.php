b4fca0f8fb7fb4c5ee715663b46a9c1767700157
太田桂輔 <>
2019-08-26 17:41:49 +0900

    論理削除機能を実装する為、Modelにて論理削除の場所を指示と論理削除の宣言を行う

--- app/Models/DailyReport.php
+++ app/Models/DailyReport.php
@@ -3,9 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class DailyReport extends Model
 {
+    use SoftDeletes;
+    
     protected $table = 'daily_reports';
 
     protected $fillable = [
