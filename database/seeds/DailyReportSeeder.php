4962dbde91aeb8a473ce62481a1644a17262779e
太田桂輔 <>
2019-09-04 17:51:15 +0900

    テスト内容の追加

--- database/seeds/DailyReportSeeder.php
+++ database/seeds/DailyReportSeeder.php
@@ -12,11 +12,27 @@ class DailyReportSeeder extends Seeder
     public function run()
     {
         DB::table('daily_reports')->insert([
+            [
             'user_id'        => 2,
             'title'          => 'タイトルテスト',
             'contents'        => '日報の内容テスト',
             'reporting_time' => '2019-8-23',
             'created_at'     => '2019-8-23',
+            ],
+            [
+            'user_id'        => 2,
+            'title'          => 'タイトルテスト',
+            'contents'        => '日報の内容テスト',
+            'reporting_time' => '2019-9-23',
+            'created_at'     => '2019-9-23',
+            ],
+            [
+            'user_id'        => 2,
+            'title'          => 'タイトルテスト',
+            'contents'        => '日報の内容テスト',
+            'reporting_time' => '2019-10-23',
+            'created_at'     => '2019-10-23',
+            ],
         ]);
     }
 }
