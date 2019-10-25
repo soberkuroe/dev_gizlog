ac9dfab3164ac61cea9532fcdd6cb2154c95851a
太田桂輔 <>
2019-10-26 08:00:13 +0900

    Modelにて論理削除の実装、ホワイトリストのオーバーライドを行う

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -1,11 +1,18 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'tag_category_id',
+        'title',
+        'content'
+    ];
 }
 
