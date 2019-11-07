8b840a0a042a0d9263f3b19dfc6e610b06b0de8d
太田桂輔 <>
2019-11-07 17:07:16 +0900

    Eagerロード利用する為、$withプロパティを指定

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
+use App\Services\SearchingScope;
 
 class Question extends Model
 {
@@ -15,8 +16,32 @@ class Question extends Model
         'title',
         'content'
     ];
-    public function tagCategory(){
+
+    protected $with =[
+        'comments',
+        'tagCategory'
+    ];
+
+    public function tagCategory()
+    {
         return $this->belongsTo('App\Models\TagCategory');
     }
-}
 
+    public function comments()
+    {
+        return $this->hasMany('App\Models\Comment');
+    }
+
+    public function fetchQuestion($userId, $serchWord)
+    {
+        if(empty($serchWord))
+        {   
+            return $this->where('user_id', $userId)->get();
+        }else
+        {
+            // return $this->where('title', 'LIKE','%' . $serchWord['search_word'] . '%')
+            //             ->orWhere('tag_category_id', '=', $serchWord['tag_category_id'])
+            //             ->get();
+        }
+    }
+}
